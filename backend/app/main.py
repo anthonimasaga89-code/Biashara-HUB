a6c996b12a30b2267073285fab3dd91af7a76aed
mtundudev <dottomtundu9@gmail.com>
2026-09-19 13:01:58 +0300

    init project setup

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.core.config import settings
+
+
+
+
+app=FastAPI(title=settings.APP_NAME)
+
+
+
+@app.get("/health")
+def HealthCheck():
+    return {
+        "status":"Ok",
+        "message":"welcome to Biashara Hub"
+    }
