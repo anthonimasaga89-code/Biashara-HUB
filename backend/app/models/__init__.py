ad902b7342d513fb51ffc5c295703a17e9bfe9b0
mtundudev <dottomtundu9@gmail.com>
2026-09-20 03:31:09 +0300

    feat:models register

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,10 @@
 #register all of your models here
 
 from app.core.database import Base
+from app.models.user import User
+from app.models.role import Role
+from app.models.revokedtoken import ReVoked
 
 
 
-__all__=["Base"]
+__all__=["Base","User","Role","ReVoked"]
