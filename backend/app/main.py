03ec3e507bc8f0accbb3e3516515f304653841e6
mtundudev <dottomtundu9@gmail.com>
2026-09-20 03:40:03 +0300

    feat:small bugs

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.core.config import settings
-from app.api.v1.router import user,seedrole
+from app.api.v1.router import user
 
 
 
@@ -8,7 +8,7 @@ from app.api.v1.router import user,seedrole
 app=FastAPI(title=settings.APP_NAME)
 
 app.include_router(user.router)
-app.include_router(seedrole.router)
+
 
 @app.get("/health",tags=["Health"])
 def HealthCheck():
--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,9 +2,8 @@
 
 from app.core.database import Base
 from app.models.user import User
-from app.models.role import Role
 from app.models.revokedtoken import ReVoked
 
 
 
-__all__=["Base","User","Role","ReVoked"]
+__all__=["Base","User","ReVoked"]
